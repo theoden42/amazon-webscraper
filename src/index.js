a4a16cfec9a7fc40256c437c01ceb900831d5de8
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2022-09-11 23:50:51 +0530

    Added basic console log for the parser

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-var axios = require("axios");
-var { parse } = require("node-html-parser");
+const axios = require("axios");
+const { parse } = require("node-html-parser");
+const cheerio = require('cheerio');
 
 
-let scrape = async (url) => {
+
+const scrape = async (url) => {
   let data = await axios.get(url, {
     headers: {
       "User-Agent":
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
     }
   });
-  console.log(data.data) // the actual data scraped from webpage
 
-  let res = parse(data.data)
-  console.log(res) // HTML Element from the parsed data   
+  const res = parse(data.data)
+  console.log(res) 
 };
 
 scrape(
-  "https://amzn.eu/d/5SZAHrl"
+    "https://www.amazon.in/Cheetos-Masala-Balls-32g/dp/B016KNUF7I/ref=sr_1_1?crid=1DY4KWJUZ9F4P&keywords=cheetos&qid=1662920314&sprefix=cheeto%2Caps%2C613&sr=8-1"
 );
